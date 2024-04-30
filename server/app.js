af5afaa5724d5bb1a8f300a4a6185806c31a21cf
Otuekong Nyong <109167028+DavyDevelops@users.noreply.github.com>
2024-04-30 01:04:06 +0000

    Pending changes exported from your codespace

--- server/app.js
+++ server/app.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require("dotenv").config({ path: "./config/config.env" });
+const express = require("express");
 const morgan = require("morgan");
-const connectDB = require('./config/db');
 
-dotenv.config({ path: "./config/.env" });
+const connectDB = require("./config/db");
+
+// const auth = require("./middlewares/auth");
 
 const app = express();
+
+// middlewares
+app.use(express.json());
 app.use(morgan("tiny"));
+app.use(require("cors")());
 
-// Middlewares
+// routes
+app.use("/api", require("./routes/auth"));
+// app.use("/api", require("./routes/contact"));
 
-// Routes
-app.use("/api", require("./routes/auth"))
-
-
-
-
-// Server configurations
+// server configurations.
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, async() => {
-    await connectDB();
-    console.log(`Server listening on port: ${PORT}`)
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server listening on port: ${PORT}`);
 });
